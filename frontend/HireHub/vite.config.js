f128d95ba7c23ae2212209915c2f4516faab3bcd
suhavijindal2003 <suhavi.jindal03@gmail.com>
2025-09-07 22:24:05 +0530

    Initialisation of frontend folder

---
+++ frontend/HireHub/vite.config.js
@@ -0,0 +1,30 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+// import tailwindcss from '@tailwindcss/vite'
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   tailwindcss()
+// })
+
+
+// // export default defineConfig({
+// //   plugins: [
+// //     tailwindcss(),
+// //   ],
+// // })
+
+
+
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    react(),
+    tailwindcss(),  // put Tailwind inside plugins array
+  ],
+})
+
